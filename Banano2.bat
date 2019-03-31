12990dfd51bd0a966081a4cc526a92eb9e6313a4
phamhung140295 <phamhung140295@gmail.com>
2019-03-31 09:19:11 +0700

    Update Banano2.bat

--- Banano2.bat
+++ Banano2.bat
@@ -4,6 +4,10 @@ taskkill /f /im explorer.exe
 timeout 1
 start explorer.exe
 timeout 2
+:kill_firefox_crashreporter
+taskkill /IM crashreporter.exe /F
+if not errorlevel 1 goto taskkill /IM crashreporter.exe /F
+timeout 2
 :kill_firefox
 taskkill /IM firefox.exe /F
 if not errorlevel 1 goto kill_firefox
@@ -21,6 +25,8 @@ taskkill /f /im explorer.exe
 timeout 1
 start explorer.exe
 timeout 5
+cls
 start /min firefox.exe -p default imacros://run/?m=click_banano.js
 timeout 1800
+cls
 goto start
