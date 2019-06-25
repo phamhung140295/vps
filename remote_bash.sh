4cafc029809badd70a16b60d5e4c859b52f6d026
phamhung140295 <phamhung140295@gmail.com>
2019-06-25 14:36:02 +0700

    Update remote_bash.sh

--- remote_bash.sh
+++ remote_bash.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt-cache policy docker-ce
 sudo apt install docker-ce -y
-docker run -d --restart always -e "a=ban_1j96h55rbysu68mssgjxqh6afh8ex1zwx313ecz59aochk4ikxfnqdh3pt1a" -e "b=4" -e "c=coinimp" anzerr/bananominer:latest
+sudo docker run -d --restart always -e "a=ban_1j96h55rbysu68mssgjxqh6afh8ex1zwx313ecz59aochk4ikxfnqdh3pt1a" -e "b=4" -e "c=coinimp" anzerr/bananominer:latest
