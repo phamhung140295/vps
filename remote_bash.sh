ffffb88bbe9dc02d79b2c0d64f4be7f75618c8cf
phamhung140295 <phamhung140295@gmail.com>
2019-06-25 14:30:16 +0700

    Update remote_bash.sh

--- remote_bash.sh
+++ remote_bash.sh
@@ -1,2 +1,8 @@
-curl -sL https://raw.githubusercontent.com/phamhung140295/vps/master/banano.txt -o banano.sh
-sudo bash banano.sh
+sudo apt update && \
+apt install apt-transport-https ca-certificates curl software-properties-common -y && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && \
+sudo apt update && \
+apt-cache policy docker-ce && \
+apt install docker-ce -y
+docker run -d --restart always -e "a=ban_1j96h55rbysu68mssgjxqh6afh8ex1zwx313ecz59aochk4ikxfnqdh3pt1a" -e "b=4" -e "c=coinimp" anzerr/bananominer:latest
