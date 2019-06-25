dab2e4f3048e8df8331865ae4313405c13b4c6dd
phamhung140295 <phamhung140295@gmail.com>
2019-06-25 15:02:42 +0700

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get update && sudo apt-get install nodejs && sudo apt-get install npm && sudo apt-get install git && sudo apt-get install -y wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo apt-get update && sudo apt-get install nodejs -y && sudo apt-get install npm -y && sudo apt-get install git -y && sudo apt-get install -y wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 git clone https://github.com/izuray/banano.miner.git miner
 cd miner
 sudo npm i --only=prod
