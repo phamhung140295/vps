42cf3306abf8efef625c86acfd81a89724d91533
phamhung140295 <phamhung140295@gmail.com>
2019-07-05 10:29:34 +0700

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 mkdir webchain
 cd webchain
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0/webchain-miner-2.7.0-linux-amd64.tar.gz
 tar zxvf webchain-miner-2.7.0-linux-amd64.tar.gz 
-./webchain-miner -o pool.webchain.network:3333 -u 0xc876877b6853f1fd73cee5d4ff5fdfc32af6d519 -p x --thread=4
+./webchain-miner -o pool.webchain.network:3333 -u 0x1a1fefb6423744deab6f430076795d5ed501a569 -p x --thread=4
