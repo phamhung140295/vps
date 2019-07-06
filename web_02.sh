7f2c9cda7bbd8236d1e0a927af61b972d6781aa0
phamhung140295 <phamhung140295@gmail.com>
2019-07-06 15:19:50 +0700

    Update web_02.sh

--- web_02.sh
+++ web_02.sh
@@ -3,4 +3,4 @@ mkdir webchain
 cd webchain
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0/webchain-miner-2.7.0-linux-amd64.tar.gz
 tar zxvf webchain-miner-2.7.0-linux-amd64.tar.gz 
-./webchain-miner -o pool.webchain.network:3333 -u 0x54b3bdd37f7d567d9cf4469fce2a51898307b1a2 -p x --thread=4
+./webchain-miner -o pool.webchain.network:3333 -u 0xefc1b7611f756a99d4fa31fb1950ae7c42de1b6e -p x --thread=4
