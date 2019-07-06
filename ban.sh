e1805584db2de5829066073ad5ab9509bc40c9cc
phamhung140295 <phamhung140295@gmail.com>
2019-07-06 17:42:03 +0700

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-THREADS=$(grep -c ^processor /proc/cpuinfo) && \
+
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && \
 sudo apt update && \
 sudo apt-get install -y gcc g++ make && \
